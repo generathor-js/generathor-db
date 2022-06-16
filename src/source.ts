9c37cb7bbd274014f8c1dfff6426b1e405863b21
Diego Michelena <diego@instacash.pe>
2022-06-15 19:27:31 -0500

    Add transformers

--- src/source.ts
+++ src/source.ts
@@ -1,20 +1,30 @@
-import {Source as BaseSource, Item} from 'generathor';
+import {Source as BaseSource} from 'generathor';
 import {Factory, FactoryInput} from './engines/factory';
+import {Item} from './engines/scanner';
 
 export type SourceConfiguration = FactoryInput;
+export type Transformer = (item: Item) => void;
 
 export class Source extends BaseSource {
   protected $items: Item[] = [];
+  protected $transformers?: Transformer[]
 
   public constructor(
-    protected $configuration: SourceConfiguration
+    protected $configuration: SourceConfiguration,
+    transformers?: Transformer[]
   ) {
     super();
+    this.$transformers = transformers || [];
   }
 
   public async load(): Promise<void> {
     const scanner = Factory.scanner(this.$configuration);
     this.$items = await scanner.scan();
+    for (const transformer of this.$transformers) {
+      for (const item of this.$items) {
+        transformer(item);
+      }
+    }
   }
 
   public items(): Item[] {
